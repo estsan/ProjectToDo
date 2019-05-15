d22e63e38d3e29cb3fb0007afa389aaecf4be922
Unknown <sandstrom.ester@gmail.com>
2019-05-15 14:18:15 +0200

    Funktion som tar bort och lägger till big-box

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+let items = new Array();
 
+// BigBox(true);
+
+let bigBox = document.querySelector("#big")
+function BigBox(isPresent) {
+    let place = document.querySelector("body");
+    let foot = document.querySelector("footer");
+    if (isPresent) {
+        bigBox.remove();
+    }
+    else {
+        place.insertBefore(bigBox, foot);
+    }
+}
+
+let checks = document.querySelector("#check-all");
+checks.onclick = event => {
+    event.preventDefault();
+    BigBox(false);
+};
+
+let clears = document.querySelector("#clear");
+clears.onclick = event => {
+    event.preventDefault();
+    BigBox(true);
+};
