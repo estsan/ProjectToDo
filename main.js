9fa6fdebe2a157bd6332b4fdd65527f47701ebdb
Unknown <sandstrom.ester@gmail.com>
2019-05-15 15:21:19 +0200

    Dagsslut

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let items = new Array();
-
-// BigBox(true);
-
 let bigBox = document.querySelector("#big")
+let addItem = document.querySelector("#text-add")
+let listItem = document.querySelector("#list-item");
+let bottomRow = document.querySelector("#bottom");
+
+listItem.remove();
+
 function BigBox(isPresent) {
     let place = document.querySelector("body");
     let foot = document.querySelector("footer");
@@ -13,6 +16,17 @@ function BigBox(isPresent) {
         place.insertBefore(bigBox, foot);
     }
 }
+
+
+addItem.addEventListener("keydown", function(e) {
+    if (e.keyCode === 13) {
+        if (items === null){
+            BigBox(false);
+        }
+        let newListItem = listItem;
+        bigBox.insertBefore(newListItem, bottomRow);
+    }
+});
 
 let checks = document.querySelector("#check-all");
 checks.onclick = event => {
@@ -25,3 +39,4 @@ clears.onclick = event => {
     event.preventDefault();
     BigBox(true);
 };
+BigBox(true);
